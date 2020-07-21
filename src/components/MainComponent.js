e3e834bddccfbb37619388bc90abee337f8846f4
vaivk4 <vai.vk4@gmail.com>
2020-07-21 17:11:59 +0530

    changes in component done

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -23,7 +23,7 @@ class Main extends Component {
       <div>
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <NavbarBrand href="/" style={{fontWeight:"bold", fontSize:"25px"}}>Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
